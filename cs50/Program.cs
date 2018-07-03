3dbc0cf1bcc57754967f7a1cec7278752d6de10e
d.trofimov <d.trofimov@cloudpayments.ru>
2018-07-03 10:17:32 +0300

    Add sorter

---
+++ cs50/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cs50
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var sorter = new Sorter();
+            var numbers = new[] { 1, 3, 5, 6, 4, 2, 9, 8, 7, 0, 0, 0 };
+            var sorted = sorter.MergeSort(numbers);
+            PrintNumbers(numbers);
+            PrintNumbers(sorted);
+            Console.ReadLine();
+        }
+
+        private static void PrintNumbers(IEnumerable<int> numbers)
+        {
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ cs50/Sorter.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Generic;
+
+namespace cs50
+{
+    public class Sorter
+    {
+        public void BubbleSort(IList<int> numbers)
+        {
+            for (var i = 0; i < numbers.Count - 1; i++)
+            {
+                for (var j = i + 1; j < numbers.Count; j++)
+                {
+                    if (numbers[i] > numbers[j])
+                    {
+                        Swap(numbers, i, j);
+                    }
+                }
+            }
+        }
+
+        public void InsertionSort(IList<int> numbers)
+        {
+            for (var i = 1; i < numbers.Count; i++)
+            {
+                var temp = numbers[i];
+                int j;
+                for (j = i - 1; j >= 0 && numbers[j] > temp; j--)
+                {
+                    numbers[j + 1] = numbers[j];
+                }
+                numbers[j + 1] = temp;
+            }
+        }
+
+        public void SelectionSort(IList<int> numbers)
+        {
+            for (var i = 0; i < numbers.Count - 1; i++)
+            {
+                var min = numbers[i];
+                var k = i;
+                for (var j = i + 1; j < numbers.Count; j++)
+                {
+                    if (numbers[j] < min)
+                    {
+                        min = numbers[j];
+                        k = j;
+                    }
+                }
+                Swap(numbers, i, k);
+            }
+        }
+
+        public int[] MergeSort(int[] numbers)
+        {
+            if (numbers.Length < 2)
+            {
+                return numbers;
+            }
+
+            var median = numbers.Length / 2;
+
+            var left = new int[median];
+            for (var i = 0; i < left.Length; i++)
+            {
+                left[i] = numbers[i];
+            }
+
+            var right = new int[numbers.Length - median];
+            for (var i = 0; i < right.Length; i++)
+            {
+                right[i] = numbers[median + i];
+            }
+
+            return Merge(MergeSort(left), MergeSort(right));
+        }
+
+        private int[] Merge(int[] left, int[] right)
+        {
+            var result = new int[left.Length + right.Length];
+
+            var i = 0;
+            var j = 0;
+            var k = 0;
+
+            while (i + j < result.Length)
+            {
+                if (i == left.Length)
+                {
+                    result[k++] = right[j++];
+                    continue;
+                }
+                if (j == right.Length)
+                {
+                    result[k++] = left[i++];
+                    continue;
+                }
+                if (left[i] < right[j])
+                {
+                    result[k++] = left[i++];
+                }
+                else
+                {
+                    result[k++] = right[j++];
+                }
+            }
+
+            return result;
+        }
+
+        private void Swap(IList<int> numbers, int i, int j)
+        {
+            var temp = numbers[i];
+            numbers[i] = numbers[j];
+            numbers[j] = temp;
+        }
+    }
+}
